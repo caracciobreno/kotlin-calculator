2aad0ce4d4acfabb2882239de90cb209f4c9dcd8
Breno Sherrer Caraccio <breno.sherrer@icloud.com>
2020-10-13 20:09:33 +0100

    change FileUtils to be a singleton instead of an abstract class

--- src/main/kotlin/libs/file/FileUtils.kt
+++ src/main/kotlin/libs/file/FileUtils.kt
@@ -2,11 +2,9 @@ package libs.file
 
 import java.io.File
 
-abstract class FileUtils {
+object FileUtils {
 
-    companion object {
-        // Reads the given file and maps each line to a string in a list. Returns null in case the file isn't readable.
-        fun readFileAsLinesOrNull(fileName: String): List<String>? =
-            File(fileName).takeIf { it.canRead() }?.useLines { it.toList() }
-    }
+    // Reads the given file and maps each line to a string in a list. Returns null in case the file isn't readable.
+    fun readFileAsLinesOrNull(fileName: String): List<String>? =
+        File(fileName).takeIf { it.canRead() }?.useLines { it.toList() }
 }
